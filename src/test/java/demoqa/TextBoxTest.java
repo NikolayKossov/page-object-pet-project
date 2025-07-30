d8ee06c4d8e40fda1a9a39c935e8a7ec5c8a1aa3
Nikolay_Kossov <nkoss2019@gmail.com>
2025-07-30 20:02:37 +0200

    tag added

--- src/test/java/demoqa/TextBoxTest.java
+++ src/test/java/demoqa/TextBoxTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static io.qameta.allure.Allure.step;
 import static pages.utils.DataHolder.*;
 
-@Tag("simple")
+@Tag("set_up")
 public class TextBoxTest extends TestBase {
 
     @Test
